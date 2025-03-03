08edfb1310e83c4a86cd4e1afb346c5701357440
unknown <gerardodfa@gmail.com>
2025-03-03 00:26:11 -0500

    cats shop parche

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function createParticle() {
     const particle = document.createElement("div");
     particle.classList.add("particle");
     particle.style.left = Math.random() * 100 + "%";
-    particle.style.animationDuration = Math.random() * 10 + 10 + "s"; // Duración aleatoria entre 5 y 10 segundos
+    particle.style.animationDuration = Math.random() * 5 + 5 + "s"; // Duración aleatoria entre 5 y 10 segundos
     particle.style.animationDelay = Math.random() * 2 + "s"; // Retraso aleatorio hasta 2 segundos
     document.getElementById("particles").appendChild(particle);
 
@@ -16,6 +16,16 @@ function createParticle() {
         particle.remove();
     }, 12000); // Tiempo suficiente para la duración y el retraso
 }
+
 // Crear partículas continuamente
 setInterval(createParticle, 500); // Crear una partícula cada 500ms
 
+// Mostrar el menú al hacer clic en el engranaje
+document.getElementById("settings-icon").addEventListener("click", function () {
+    const menu = document.getElementById("settings-menu");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+});
